2274c175a092dab35cda1bfcf5fd19abe01ca05c
Andrew Lin <linysh1123@gmail.com>
2025-07-12 15:42:53 -0700

    update Dockerfile and startup scripts

--- docker-build/app_files/docker_entrypoint.sh
+++ docker-build/app_files/docker_entrypoint.sh
@@ -8,10 +8,9 @@ echo "✅ Running docker_entrypoint.sh ..."
 
 export USER=root
 export PYTHONDONTWRITEBYTECODE=1
-#export TRANSFORMERS_CACHE="$(pwd)/third_party/hub"
 export HF_HOME=/workspace/SongGeneration/
 export NCCL_HOME=/usr/local/tccl
-export PYTHONPATH="$(pwd)/codeclm/tokenizer/:$(pwd):$(pwd)/codeclm/tokenizer/Flow1dVAE/:$(pwd)/codeclm/tokenizer/:$PYTHONPATH"
+export PYTHONPATH="/workspace/repo/songgeneration/codeclm/tokenizer/:/workspace/repo/songgeneration:/workspace/repo/songgeneration/codeclm/tokenizer/Flow1dVAE/:/workspace/repo/songgeneration/codeclm/tokenizer/:$PYTHONPATH"
 
 echo "✅ Environment variables set."
 echo ""
@@ -21,6 +20,18 @@ echo "  HF_HOME  = ${HF_HOME}"
 echo "  NCCL_HOME  = ${NCCL_HOME}"
 echo "  PYTHONPATH  = ${PYTHONPATH}"
 echo ""
+
+# ---------------------------------------------------
+# Check if repo exists
+# ---------------------------------------------------
+
+if [ ! -d "/workspace/repo/songgeneration/.git" ]; then
+    echo "🚀 Cloning repo into /workspace/repo/songgeneration ..."
+    mkdir -p /workspace/repo
+    git clone https://github.com/AndrewYukon/songgeneration /workspace/repo/songgeneration
+else
+    echo "✅ Repo already exists at /workspace/repo/songgeneration"
+fi
 
 # ---------------------------------------------------
 # Create required directories
@@ -33,10 +44,10 @@ mkdir -p /workspace/SongGeneration/output
 # Create symlinks
 # ---------------------------------------------------
 
-ln -sf /workspace/SongGeneration/ckpt /repo/songgeneration/ckpt
-ln -sf /workspace/SongGeneration/third_party /repo/songgeneration/third_party
-ln -sf /workspace/SongGeneration/jsonl /repo/songgeneration/jsonl
-ln -sf /workspace/SongGeneration/output /repo/songgeneration/output
+ln -sf /workspace/SongGeneration/ckpt /workspace/repo/songgeneration/ckpt
+ln -sf /workspace/SongGeneration/third_party /workspace/repo/songgeneration/third_party
+ln -sf /workspace/SongGeneration/jsonl /workspace/repo/songgeneration/jsonl
+ln -sf /workspace/SongGeneration/output /workspace/repo/songgeneration/output
 
 echo "✅ Directories and symlinks prepared."
 
@@ -65,12 +76,9 @@ echo "Example:"
 echo "python3 generate.py /workspace/SongGeneration/ckpt /workspace/SongGeneration/jsonl/test.jsonl /workspace/SongGeneration/output"
 echo "------------------------------------------------------------"
 
-# Start an interactive shell
-# exec /bin/bash
+# Switch to repo workdir
+cd /workspace/repo/songgeneration
 
-# keep container running
-#echo "✅ docker_entrypoint.sh finished, keeping container alive."
-#exec tail -f /dev/null
 if [ $# -eq 0 ]; then
     echo "✅ docker_entrypoint.sh finished, keeping container alive..."
     exec tail -f /dev/null
